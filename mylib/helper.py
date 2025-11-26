5c6b1700b0e455eecf95997b6fa710a0e8b6eb4a
Triv24 <triveni01gauri@gmail.com>
2025-11-26 19:51:17 +0530

    Added the initialisation ofo session_states for models and vectorDBs, Updated the requirements.txt file accordingly

--- mylib/helper.py
+++ mylib/helper.py
@@ -1,10 +1,24 @@
 # ------------------- Importing required functionalities ------------------------------
 import streamlit as st
-from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_google_genai import ChatGoogleGenerativeAI, GoogleGenerativeAIEmbeddings
 from langchain_core.prompts import PromptTemplate
-from langchain_core.output_parsers import StrOutputParser
+from langchain_core.output_parsers import StrOutputParser, JsonOutputParser
 from langchain_core.tools import tool
+from langchain_chroma import Chroma
 from docx import Document
+
+# ---------------------------- Session Initialisation ---------------------------------
+def init_session():
+    if "llm" not in st.session_state:
+        st.session_state["llm"] = get_llm()
+    if "embedding_model" not in st.session_state:
+        st.session_state["embedding_model"] = get_embedding_model()
+    if "my_store" not in st.session_state:
+        st.session_state["my_store"] = get_vector_store()
+    if "string_parser" not in st.session_state:
+        st.session_state["string_parser"] = get_string_output_parser()
+    if "json_parser" not in st.session_state:
+        st.session_state["json_parser"] = get_json_output_parser()
 
 # --------------------------- Initialising Models --------------------------------------
 def get_llm():
@@ -14,11 +28,26 @@ def get_llm():
     )
     return llm
 
-def get_db():
-    pass 
+def get_embedding_model():
+    embedding_model = GoogleGenerativeAIEmbeddings(
+        model = "gemini-embedding-001",
+        google_api_key=st.secrets["GEMINI_API_KEY"]
+    )
+    return embedding_model
 
-def get_output_parser ():
+def get_vector_store():
+    chroma_client = Chroma(
+        embedding_function=st.session_state.embedding_model,
+        persist_directory="Textbooks",
+        collection_name="textbook_data"
+    ) 
+    return chroma_client
+
+def get_string_output_parser ():
     return StrOutputParser()
+
+def get_json_output_parser ():
+    return JsonOutputParser()
 
 # ---------------------- Tools for the Agent -------------------------------------------
 @tool
@@ -50,7 +79,19 @@ def solve_assignment(assignment) :
 def generate_docx():
     pass
 
+def create_knowledge_base(file):
+    pass
+
 # ----------------------------- PROMPTS --------------------------------------------------
+
+model_question_prompt = PromptTemplate(
+    template="""
+    
+    """,
+    input_variables=[]
+)
+
+
 
 # ------------------------------- RAG essentials -----------------------------------------
 def add_knowledge_base():
