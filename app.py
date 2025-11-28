a5ed291c5011fa4c99d0c13bff51c5fd9e6ce7a8
Triv24 <triveni01gauri@gmail.com>
2025-11-28 11:28:48 +0530

    Added functionalites

--- app.py
+++ app.py
@@ -1,11 +1,8 @@
 import streamlit as st
 import tempfile
-import os
-from mylib.helper import init_session, get_llm, load_pdf, extract_questions, get_model_questions, get_docx
+from mylib.helper import load_pdf, extract_questions, get_model_questions, get_docx, get_chunks, store_in_db, get_relevant_chunks, explain_concept
 from langchain.agents import create_agent
 from langchain_google_genai import ChatGoogleGenerativeAI
-
-init_session()
 
 # Introductory content
 st.markdown("""
@@ -47,10 +44,6 @@ with model_qp:
     if loaded_qp_content :
         with st.spinner("Starting Prediction..."):
 
-            llm = ChatGoogleGenerativeAI(
-                model = "gemini-2.5-flash",
-                google_api_key = st.secrets["GEMINI_API_KEY"]
-            )
             st.session_state.agent = create_agent(
                 model=ChatGoogleGenerativeAI(
                     model = "gemini-2.5-flash",
@@ -64,7 +57,7 @@ with model_qp:
             response = agent.invoke({
                 "messages" : [{
                     "role" : "user",
-                    "content" : f"Please predict the model question paper for the question papers content : {loaded_qp_content}. Ultimately i need the file path of the word document that contains the model question papers. Just give me the final file path in string format. Do not add unnecessary words in output, the output string should wholly represent the file path only."
+                    "content" : f"Please predict the model question paper for the question papers content : {loaded_qp_content}. first extract the clear questions from the content using a tool, then use that extracted questions for analysis and prediction for model question paper. Ultimately i need the file path of the word document that contains the model question papers. Just give me the final file path in string format. Do not add unnecessary words in output, the output string should wholly represent the file path only."
                 }]
             })
             
@@ -78,3 +71,113 @@ with model_qp:
                     mime="application/vnd.openxmlformats-officedocument.wordprocessingml.document"
                 )
 
+# ----------------------------------------------------------------------------------------------------------------------------
+#  Concepts Explaining Assistant
+# ----------------------------------------------------------------------------------------------------------------------------
+
+with concepts:
+    st.write("Please upload your Textbook : ")
+    file = st.file_uploader("Upload here...", type=".pdf")
+    if file is not None :
+
+        if "file" not in st.session_state:
+            with st.spinner("Loading contents will take time as the file is very large...") :
+                with tempfile.NamedTemporaryFile(delete=False, suffix=".pdf") as tmp :
+                    tmp.write(file.getbuffer())
+                    temp_pdf_path = tmp.name
+                st.session_state.text = load_pdf(temp_pdf_path)
+            st.session_state.file = temp_pdf_path
+            st.success("Loaded content successfully...")
+
+
+            
+            with st.spinner("Adding to knowledge base..."):
+                text = st.session_state.text
+                if text :
+                    chunks = get_chunks(text)
+                    store_in_db(chunks)
+            st.success("Stored succesfully")
+            st.session_state.stored = True
+
+    
+        if "llm" not in st.session_state:
+            st.session_state.llm = ChatGoogleGenerativeAI(
+                model = "gemini-2.5-flash",
+                google_api_key = st.secrets["GEMINI_API_KEY"]
+            )
+        
+        if "agent" not in st.session_state:
+            st.session_state.agent = create_agent(
+                model = st.session_state.llm,
+                tools=[get_relevant_chunks, explain_concept],
+                system_prompt="You are a helpful student assistant and a master explainer who gives suitable real world examples just by taking the contexts from textbook. Just ensure the final output is in markdown format. Make good use of markdown visual elements."
+            )
+
+        st.session_state.query = st.text_input("What concept would you like to learn ?")
+        if st.session_state.query :
+            with st.spinner("Generating response...") :
+                agent = st.session_state.agent
+                query = st.session_state.query
+                
+                prompt = f"You are a helpful study assistant. You are given a user query and you have to first collect the relevant chunks from the vector store and then based on the relevant chunks, you have to explain the concepts mentioned in the query. give the final output in string of markdown format. If you do not get any relevant chunks you can answer based on your own knowledge about the subject.\nquery : {query}"
+
+                response = agent.invoke(
+                    {
+                        "messages" : [
+                            {
+                                "role" : "user",
+                                "content" : prompt
+                            }
+                        ]
+                    }
+                )
+            
+            st.markdown(response["messages"][-1].content[0]['text'])
+
+# ----------------------------------------------------------------------------------------------------------------------------
+#  Assignments Helper
+# ----------------------------------------------------------------------------------------------------------------------------
+
+with assignments :
+    st.write("\n")
+    st.session_state.selected = st.selectbox("What is the file-type of assignments ?", ["PDF", "Plain Text"])
+
+    if st.session_state.selected == "PDF" :
+        file = st.file_uploader("Upload the PDF here...", type = ".pdf")
+        if file is not None:
+            if "file" not in st.session_state:
+                with st.spinner("Loading contents will take time as the file is very large...") :
+                    with tempfile.NamedTemporaryFile(delete=False, suffix=".pdf") as tmp :
+                        tmp.write(file.getbuffer())
+                        temp_pdf_path = tmp.name
+                        st.session_state.text = load_pdf(temp_pdf_path)
+                st.session_state.file = temp_pdf_path
+                st.success("Loaded content successfully...")
+
+    elif (st.session_state.selected == "Plain Text") :
+        st.session_state.text = st.text_input("Paste the text Here...")
+        if "text" not in st.session_state or st.session_state.text.strip() == "":
+            st.warning("Please Insert text")
+    
+    if "text" in st.session_state :
+        st.session_state.words = st.slider("Please set the approximate number of words for the size of the answers", min_value=50, max_value=500, step=50,)
+
+    if "words" in st.session_state :
+        with st.spinner("Processing...") :
+            if "llm" not in st.session_state:
+                st.session_state.llm = ChatGoogleGenerativeAI(
+                    model = "gemini-2.5-flash",
+                    google_api_key = st.secrets["GEMINI_API_KEY"]
+                )
+
+            response = st.session_state.llm.invoke(
+                    f"""
+    You are a helpful study assistant. Please solve all the given questions in standard answer formats each in about {st.session_state.words} words.
+    Directly start from 1st question.
+    Do not repeat the qeustoins in response.
+    Just give the sequence number of question and then immediately the answer.
+                    Questions : {st.session_state.text}
+                    """
+                )
+
+            st.markdown(response.content)
--- mylib/helper.py
+++ mylib/helper.py
@@ -2,8 +2,20 @@ import streamlit as st
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.tools import tool
+from langchain_core import documents
+from langchain_core.prompts import PromptTemplate
+from langchain_chroma import Chroma
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_core.output_parsers import StrOutputParser
 from docx import Document
 import tempfile
+
+
+
+# ------------------------------------------------------------------------------------------------------------------------------
+# Helper Functions
+# ------------------------------------------------------------------------------------------------------------------------------
 
 def init_session():
     if "llm" not in st.session_state:
@@ -21,6 +33,10 @@ def load_pdf(file):
         full_text += doc.page_content
 
     return full_text
+
+# ------------------------------------------------------------------------------------------------------------------------------
+# Tools for Model Question Paper Generator
+# ------------------------------------------------------------------------------------------------------------------------------
 
 @tool
 def extract_questions(qp):
@@ -59,29 +75,17 @@ def get_model_questions(question_sets : str) -> str :
     ---
     INSTRUCTIONS
     Carefully read and analyze the full contents of the previous question papers provided.
-
-    Identify recurring patterns, frequently tested concepts, question styles, difficulty levels, and the distribution of marks.
-
+    Identify recurring patterns, frequently tested concepts, question styles, difficulty levels.
     Based on this analysis, estimate and construct a complete model question paper that closely reflects what is most likely to appear in the upcoming exam.
-
     Ensure the model paper:
-
     Follows the same structure and format as typical past exams
-
-    Covers all major topics proportionately
-
-    Uses clear, academic language
-
     Includes appropriate sectioning (Part A, Part B, etc., if relevant)
     ---
     CONTEXT
-
     Previous question paper content:
     {question_sets}
-
     ---
     OUTPUT
-
     A well-structured string, all questions for model question paper that represents the best possible estimation of upcoming exam questions.
     directly start from Q1
     """
@@ -114,4 +118,139 @@ def get_llm():
     )
 
     return llm
+
+# --------------------------------------------------------------------------------------------------------------
+# Concepts Explaining Assistant Tools 
+# --------------------------------------------------------------------------------------------------------------
+
+def get_chunks(text_content):
+    """
+    This tool takes the loaded content from the pdf of the textbook as arguement and splits them into chunks. Then it converts the text chunks in documents and then returns the list of document chunks.
+    """
+
+    # Text Splitter
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size = 1500,
+        chunk_overlap = 10
+    )
+
+    chunks = text_splitter.split_text(text_content)
+
+    document_chunks = [documents.Document(page_content=chunk) for chunk in chunks]
+
+    return document_chunks
+
+def store_in_db(document_chunks) :
+    # Embedding function
+    embedder = HuggingFaceEmbeddings(
+        model_name = "sentence-transformers/all-MiniLM-L6-v2",
+    model_kwargs = {
+        "device": "cpu"
+    },
+    encode_kwargs = {
+        'normalize_embeddings': True
+    }
+    )
+
+    # Vector store
+    db = Chroma(
+        embedding_function=embedder,
+        persist_directory="my_knowledge_base",
+        collection_name="textbook_data"
+    )
+
+    db.add_documents(document_chunks)
+
+    return db
+
+@tool
+def get_relevant_chunks(query : str):
+    """
+    This tool takes the query as arguements and then returns the top 5 relevant results of similarity search from the vector store. The top 5 relevant chunks are returned in the form of plain string.
+    """
+    embedder = HuggingFaceEmbeddings(
+        model_name = "sentence-transformers/all-MiniLM-L6-v2",
+    model_kwargs = {
+        "device": "cpu"
+    },
+    encode_kwargs = {
+        'normalize_embeddings': True
+    }
+    )
+
+    db = Chroma(
+        embedding_function=embedder,
+        persist_directory="my_knowledge_base",
+        collection_name="textbook_data"
+    )
+
+    relevant_chunks = ""
+
+    for chunk in db.similarity_search(query, k=5):
+        relevant_chunks += chunk.page_content
+
+    return relevant_chunks
+
+@tool
+def explain_concept(relevant_chunks : str, query : str):
+    """
+    This tool takes relevant chunks from the vector store and the query as arguements and return the generated explaination of the concept mentioned in query with suitable examples.
+    """
+
+    llm = ChatGoogleGenerativeAI(
+        model = "gemini-2.5-flash",
+        google_api_key = st.secrets["GEMINI_API_KEY"]
+    )
+
+    parser = StrOutputParser()
+
+    prompt = PromptTemplate(
+        template="""
+        You are an expert educator, curriculum designer, and AI explanation specialist trained to teach complex topics in simple, clear, student-friendly language, using real-life examples and context-aware reasoning.
+
+INSTRUCTIONS (READ CAREFULLY BEFORE RESPONDING)
+
+Use information found in the relevant chunks provided:
+you can add some extra info only if needed
+{relevant_chunks}
+
+You must explain the concept asked in the user query:
+{query}
+
+Your explanation must follow these rules:
+
+Use very simple language, suitable for a high-school or first-year college student.
+
+Break the explanation into clear sections and step-by-step reasoning.
+
+Add real-life analogies, examples, or stories that make the concept easy to understand.
+
+Avoid jargon. If a technical term is required, define it clearly.
+
+Stay fully grounded in the content of the provided chunks; do not invent unsupported information.
+
+Always end with a short “Why this concept matters” section.
+
+If any part of the query cannot be answered from the chunks, you must clearly state which part is missing.
+
+output format should be string of answer in markdown.
+        """,
+        input_variables=["query", "relevant_chunks"]
+    )
+
+    chain = prompt | llm | parser
+    
+    response = chain.invoke(
+        {
+            "query" : query,
+            "relevant_chunks" : relevant_chunks
+        }
+    )
+
+    return response
+
+    
+# --------------------------------------------------------------------------------------------------------------
+# Assignments Helper
+# --------------------------------------------------------------------------------------------------------------
 
